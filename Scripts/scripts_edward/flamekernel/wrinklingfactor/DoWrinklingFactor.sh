81a294ae2877f260c5a7f2b255f7fbc6d1a4412d
Stobers <hunt.edward@gmail.com>
2021-05-11 16:53:23 +0100

    minor script changes and naming convention

---
+++ Scripts/scripts_edward/flamekernel/wrinklingfactor/DoWrinklingFactor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export isoval=0.9
+export fuel=H2
+export dim=2
+
+rm -r *plt?????_prog* area.dat *~ Backtrace* int_prog.dat wrinkling_factor.csv;
+
+for i in plt?????;
+do
+    ./plotProg${dim}d.gnu.ex infile= $i;
+    ./isosurface${dim}d.gnu.ex inputs \
+		comps=1 \
+		isoCompName=prog_${fuel} \
+		isoVal=${isoval} \
+		infile=${i}_prog
+done
+./areaMEF${dim}d.gnu.ex inputs infile= *.mef;
+./AmrDeriveProgIntegration${dim}d.Linux.g++.gfortran.ex infile= plt?????_prog vars=prog_${fuel};
+python wf.py;
+
+rm -r plt?????_prog* area.dat int_prog.dat;
