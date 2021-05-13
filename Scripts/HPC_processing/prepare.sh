9e33252f2438d3384aebed928e5f00221123a675
Stobers <hunt.edward@gmail.com>
2021-05-13 09:07:37 +0100

    mpi typo fix

--- Scripts/HPC_processing/prepare.sh
+++ Scripts/HPC_processing/prepare.sh
@@ -9,5 +9,5 @@ ln -s ~/src/PeleAnalysis_Aspden/Src/Decimation/MyQSlim/qslim3d.gnu.ex .;
 ln -s ~/src/PeleAnalysis_Aspden/Src/sampleStreamlines3d.gnu.MPI.ex .;
 ln -s ~/src/PeleAnalysis_Aspden/Src/stream3d.gnu.MPI.ex .;
 ln -s ~/src/PeleAnalysis_Aspden/Src/streamTubeStats3d.gnu.ex .;
-ln -s ~/src/PeleAnalysis_Aspden/Src/surfMEFtoDAT3d.gnu.MPI.ex .;
-ln -s ~/src/PeleAnalysis_Aspden/Src/surfMEFtoDATbasic3d.gnu.MPI.ex .;
+ln -s ~/src/PeleAnalysis_Aspden/Src/surfMEFtoDAT3d.gnu.ex .;
+ln -s ~/src/PeleAnalysis_Aspden/Src/surfMEFtoDATbasic3d.gnu.ex .;
