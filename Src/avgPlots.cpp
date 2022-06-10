536e197d3bcd9923b5ba8095b794fd78c4ae412b
Thomas Howarth <T.Howarth2@newcastle.ac.uk>
2022-06-10 09:56:57 +0100

    flushing grids differently

--- Src/avgPlots.cpp
+++ Src/avgPlots.cpp
@@ -101,9 +101,11 @@ main (int   argc,
     Print() << "... avgMF created" << std::endl;
     Real time0 = amrData0.Time();
     Vector<std::string> names(nComp);
-    for (int i=0; i<nComp; ++i)
+    for (int i=0; i<nComp; ++i) {
       names[i] = amrData0.PlotVarNames()[comps[i]];
-      
+      amrData0.FlushGrids(comps[i]); //do i need to flush if not filled?
+    }
+    
     Real timeFinal = 0;
     Print() << "avgPlotfile setup completed." << std::endl;
     Print() << "Starting averaging... " << std::endl;
@@ -123,14 +125,16 @@ main (int   argc,
       Print() << "... loading " << infiles[iFile] << std::endl;
       // load data
       amrData.FillVar(*fileData,finestLevel,amrData.PlotVarNames(),comps);
+      for (int i = 0; i < nComp; i++) {
+	  amrData.FlushGrids(comps[i]);
+      }
       Print() << "... " << infiles[iFile] << " loaded" << std::endl;
       Print() << "... adding to averaged MF" << std::endl;
       MultiFab::Saxpy(avgMF,1.0/numFiles,*fileData,0,0,nComp,0);
       if(iFile == numFiles-1) {
 	timeFinal = amrData.Time();
       }
-      amrData.FlushGrids();
-      //Print() << "... deleting fileData" << std::endl;
+      Print() << "... deleting fileData" << std::endl;
       delete fileData;
 	        
     }
@@ -146,53 +150,57 @@ main (int   argc,
       Print() << "stdDevPlotfile setup..." << std::endl;
       Print() << "... creating stdDevMF" << std::endl;
       MultiFab stdDevMF(newba,DistributionMapping(newba),nComp,0);
-     stdDevMF.setVal(0);
-     Print() << "... stdDevMF created" << std::endl;
-     Vector<std::string> names_stddev(nComp);
-     for (int i=0; i<nComp; ++i)
-       names_stddev[i] = amrData0.PlotVarNames()[comps[i]]+"_stdDev";
-     
-     Print() << "stdDevPlotfile setup completed." << std::endl;
-     Print() << "Starting deviations... " << std::endl;
-     for (int iFile = 0; iFile<numFiles; iFile++) {
-       // Set up for reading pltfile
-       DataServices dataServices(infiles[iFile], fileType);
-       
-       if (!dataServices.AmrDataOk())
-	 DataServices::Dispatch(DataServices::ExitRequest, NULL);
-       
-       AmrData& amrData = dataServices.AmrDataRef();
-       
+      stdDevMF.setVal(0);
+      Print() << "... stdDevMF created" << std::endl;
+      Vector<std::string> names_stddev(nComp);
+      for (int i=0; i<nComp; ++i)
+	names_stddev[i] = amrData0.PlotVarNames()[comps[i]]+"_stdDev";
+      
+      Print() << "stdDevPlotfile setup completed." << std::endl;
+      Print() << "Starting deviations... " << std::endl;
+      for (int iFile = 0; iFile<numFiles; iFile++) {
+	// Set up for reading pltfile
+	DataServices dataServices(infiles[iFile], fileType);
+	
+	if (!dataServices.AmrDataOk())
+	  DataServices::Dispatch(DataServices::ExitRequest, NULL);
+	
+	AmrData& amrData = dataServices.AmrDataRef();
+	
        // make space
-       MultiFab* fileData;
-       fileData = new MultiFab(newba,DistributionMapping(newba),nComp,0);
-       Print() << "... loading " << infiles[iFile] << std::endl;
-       // load data
-       amrData.FillVar(*fileData,finestLevel,amrData.PlotVarNames(),comps);
-       Print() << "... " << infiles[iFile] << " loaded" << std::endl;
-       Print() << "... subtracting averaged MF" << std::endl;       
-       MultiFab::Subtract(*fileData,avgMF,0,0,nComp,0);
-       Print() << "... multiply with self" << std::endl;
-       MultiFab::Multiply(*fileData,*fileData,0,0,nComp,0);
-       Print() << "... add to stdDevMF" << std::endl;
-       MultiFab::Saxpy(stdDevMF,1.0/numFiles,*fileData,0,0,nComp,0);
-       amrData.FlushGrids();
-       delete fileData;
-     }
-     Print() << "Variation calculated, square rooting..." << std::endl;
-     //is this the best way to do this?
-     for (MFIter mfi(stdDevMF,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
-       const Box& bx = mfi.tilebox();
-       Array4<Real> array = stdDevMF.array(mfi);
-       AMREX_PARALLEL_FOR_4D (bx, nComp, i, j, k, n,
-			      {
-				array(i,j,k,n) = std::sqrt(array(i,j,k,n));
-			      });
-     }
-     Print() << "Finished calculating stardard deviation." << std::endl;
-     Print() << "Writing " << stdDevOutfile << "..." << std::endl;
-     WriteSingleLevelPlotfile(stdDevOutfile,stdDevMF,names_stddev,geoms,timeFinal-time0,levelSteps);
-     
+	MultiFab* fileData;
+	fileData = new MultiFab(newba,DistributionMapping(newba),nComp,0);
+	Print() << "... loading " << infiles[iFile] << std::endl;
+	// load data
+	amrData.FillVar(*fileData,finestLevel,amrData.PlotVarNames(),comps);
+	for (int i = 0; i < nComp; i++) {
+	  amrData.FlushGrids(comps[i]);
+	}
+
+	Print() << "... " << infiles[iFile] << " loaded" << std::endl;
+	Print() << "... subtracting averaged MF" << std::endl;       
+	MultiFab::Subtract(*fileData,avgMF,0,0,nComp,0);
+	Print() << "... multiply with self" << std::endl;
+	MultiFab::Multiply(*fileData,*fileData,0,0,nComp,0);
+	Print() << "... add to stdDevMF" << std::endl;
+	MultiFab::Saxpy(stdDevMF,1.0/numFiles,*fileData,0,0,nComp,0);
+	Print() << "... deleting fileData" << std::endl;
+	delete fileData;
+      }
+      Print() << "Variation calculated, square rooting..." << std::endl;
+      //is this the best way to do this?
+      for (MFIter mfi(stdDevMF,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+	const Box& bx = mfi.tilebox();
+	Array4<Real> array = stdDevMF.array(mfi);
+	AMREX_PARALLEL_FOR_4D (bx, nComp, i, j, k, n,
+			       {
+				 array(i,j,k,n) = std::sqrt(array(i,j,k,n));
+			       });
+      }
+      Print() << "Finished calculating stardard deviation." << std::endl;
+      Print() << "Writing " << stdDevOutfile << "..." << std::endl;
+      WriteSingleLevelPlotfile(stdDevOutfile,stdDevMF,names_stddev,geoms,timeFinal-time0,levelSteps);
+      
     }
     
     }
