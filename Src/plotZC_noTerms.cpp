68469d8bb7e8c00f11888694c929c13e4aa4b4e5
Thomas <T.Howarth2@newcastle.ac.uk>
2023-04-13 10:35:28 +0100

    removed extras from ZC

--- Src/plotZC_noTerms.cpp
+++ Src/plotZC_noTerms.cpp
@@ -79,11 +79,13 @@ main (int   argc,
     int idRhoin = -1;
     int idTin = -1;
     int idVortin = -1;
+    int idProdin = -1;
     //Vector<std::string> spec_names = GetSpecNames();
     const Vector<std::string>& plotVarNames = amrData.PlotVarNames();
     const std::string FCRName = "I_R("+fuelName+")";//"_ConsumptionRate";
     const std::string spName= "Y("+fuelName+")";
     const std::string oxName= "Y(O2)";
+    const std::string prodName= "Y(H2O)";
     const std::string rhoName = "density";
     const std::string tName = "temp";
     const std::string vortName = "mag_vort";
@@ -95,11 +97,12 @@ main (int   argc,
       if (plotVarNames[i] == rhoName) idRhoin = i;
       if (plotVarNames[i] == tName) idTin = i;
       if (plotVarNames[i] == vortName) idVortin = i;
+      if (plotVarNames[i] == prodName) idProdin = i;
     }
-    if (idFin<0 || idOin<0 ||  idRhoin < 0 || idTin < 0 || idFCRin < 0 || idVortin < 0)
+    if (idFin<0 || idOin<0 ||  idRhoin < 0 || idTin < 0 || idFCRin < 0 || idVortin < 0 || idProdin < 0)
       Abort("Cannot find required data in pltfile");
-    const int nCompIn  = 6;
-    int nCompOut = 4;
+    const int nCompIn  = 3;
+    int nCompOut = 3;
     
     Vector<std::string> outNames(nCompOut);
     Vector<std::string> inNames(nCompIn);
@@ -107,40 +110,51 @@ main (int   argc,
     //in
     const int idFlocal = 0; // fuel in here
     const int idOlocal = 1; // O2 in here
-    const int idRholocal = 2; // density in here
-    const int idFCRlocal = 3; //FCR in here
-    const int idTlocal = 4; //Temp in here
-    const int idVortlocal = 5;
+    //const int idRholocal = 2; // density in here
+    //const int idFCRlocal = 2; //FCR in here
+    const int idTlocal = 2; //Temp in here
+    //const int idVortlocal = 5;
+    //const int idProdlocal = 6;
     //out
+    //const int idZFOlocal = 0; // Z out here
+    //const int idZFPlocal = 1; // Z out here
+    //const int idZOPlocal = 2; // Z out here
     const int idZlocal = 0; // Z out here
-    const int idClocal = 1; // C out here
-    const int idCsourcelocal = 2; //C source here
-    const int idDalocal = 3;
+    const int idClocal = 1;//3; // C out here
+    //const int idCsourcelocal = 2;//4; //C source here
+    //const int idDalocal = 3;//5;
+    const int idfuelTempProductlocal = 2;//6;
     //const int idPhilocal = 4;
     destFillComps[idFlocal] = idFlocal;
     destFillComps[idOlocal] = idOlocal;
-    destFillComps[idRholocal] = idRholocal;
-    destFillComps[idFCRlocal] = idFCRlocal;
+    //destFillComps[idRholocal] = idRholocal;
+    //destFillComps[idFCRlocal] = idFCRlocal;
     destFillComps[idTlocal] = idTlocal;
-    destFillComps[idVortlocal] = idVortlocal;
+    //destFillComps[idVortlocal] = idVortlocal;
+    //destFillComps[idProdlocal] = idProdlocal;
     inNames[idFlocal] = spName;
     inNames[idOlocal] =  oxName;
-    inNames[idRholocal] = rhoName;
-    inNames[idFCRlocal] = FCRName;
+    //inNames[idRholocal] = rhoName;
+    //inNames[idFCRlocal] = FCRName;
     inNames[idTlocal] = tName;
-    inNames[idVortlocal] = vortName;
+    //inNames[idVortlocal] = vortName;
+    //inNames[idProdlocal] = prodName;
     outNames[idZlocal] = "mixture_fraction";
+    //outNames[idZFPlocal] = "mixture_fraction_FP";
+    //outNames[idZOPlocal] = "mixture_fraction_OP";
     outNames[idClocal] = "progress_variable";
-    outNames[idCsourcelocal] = "progress_variable_source";
-    outNames[idDalocal] = "log(Da)";
+    //outNames[idCsourcelocal] = "progress_variable_source";
+    //outNames[idDalocal] = "log(Da)";
+    outNames[idfuelTempProductlocal] = "fuel * temp";
+    Real TempIgn = 800; pp.query("IgnitionTemp",TempIgn);
+    Real fuelNorm = 1; pp.query("fuelNormalise",fuelNorm);
+    Real tempNorm = 1500; pp.query("tempNormalise",tempNorm);
     //outNames[idPhilocal] = "phi";
     Vector<std::unique_ptr<MultiFab>> outdata(Nlev);
-    Vector<MultiFab*> baseComps(Nlev);
     Vector<MultiFab*> indata(Nlev);
     Vector<Geometry> geoms(Nlev);
     RealBox rb(&(amrData.ProbLo()[0]),&(amrData.ProbHi()[0]));
     const int nGrow = 1;
-    int b[3] = {1, 1, 1};
     Real eps = 1e-8;
     for (int lev=0; lev<Nlev; ++lev)
     {
@@ -162,19 +176,20 @@ main (int   argc,
         Array4<Real> const& ZC = (*outdata[lev]).array(mfi);
         AMREX_PARALLEL_FOR_3D ( bx, i, j, k,
         {
-	  
 	  ZC(i,j,k,idZlocal) = (s*inbox(i,j,k,idFlocal) - inbox(i,j,k,idOlocal) + Y_O_air)/(s+Y_O_air);
-	  if (inbox(i,j,k,idTlocal) < Tmin || ZC(i,j,k,idZlocal) < eps) {
-	    ZC(i,j,k,idClocal) = 0;
-	  } else if (ZC(i,j,k,idZlocal) <= Z_st) {
-	    ZC(i,j,k,idClocal) = 1-inbox(i,j,k,idFlocal)/ZC(i,j,k,idZlocal);
-	    ZC(i,j,k,idCsourcelocal) = -inbox(i,j,k,idFCRlocal)/ZC(i,j,k,idZlocal);
-	  } else {
-	    ZC(i,j,k,idClocal) = ((1-Z_st)/Z_st)*(ZC(i,j,k,idZlocal)-inbox(i,j,k,idFlocal))/(1-ZC(i,j,k,idZlocal));
-	    ZC(i,j,k,idCsourcelocal) = -((1-Z_st)/Z_st)*inbox(i,j,k,idFCRlocal)/(1-ZC(i,j,k,idZlocal));
-	  }
-	  ZC(i,j,k,idDalocal) = std::log10(std::max(ZC(i,j,k,idCsourcelocal)/(inbox(i,j,k,idRholocal)*inbox(i,j,k,idVortlocal)),1e-10));
-	  //ZC(i,j,k,idPhilocal) = S*ZC(i,j,k,idZlocal)/(1-ZC(i,j,k,idZlocal));
+	  if (ZC(i,j,k,idZlocal) <= Z_st)
+	    {
+	      ZC(i,j,k,idClocal) = 1-inbox(i,j,k,idFlocal)/ZC(i,j,k,idZlocal);
+	      //ZC(i,j,k,idCsourcelocal) = -inbox(i,j,k,idFCRlocal)/ZC(i,j,k,idZlocal);
+	    } else
+	    {
+	      ZC(i,j,k,idClocal) = ((1-Z_st)/Z_st)*(ZC(i,j,k,idZlocal)-inbox(i,j,k,idFlocal))/(1-ZC(i,j,k,idZlocal));
+	      //ZC(i,j,k,idCsourcelocal) = -((1-Z_st)/Z_st)*inbox(i,j,k,idFCRlocal)/(1-ZC(i,j,k,idZlocal));
+	    }
+	  ZC(i,j,k,idClocal) = std::max(0.0,ZC(i,j,k,idClocal));
+	  ZC(i,j,k,idClocal) = std::min(1.0,ZC(i,j,k,idClocal));
+	  //ZC(i,j,k,idDalocal) = std::log10(std::max(ZC(i,j,k,idCsourcelocal)/(inbox(i,j,k,idRholocal)*inbox(i,j,k,idVortlocal)),1e-10));
+	  ZC(i,j,k,idfuelTempProductlocal) = inbox(i,j,k,idFlocal)*inbox(i,j,k,idTlocal);
         });
 	
       }
