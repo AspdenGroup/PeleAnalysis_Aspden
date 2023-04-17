9db70e39327b1dfa03e95b735aae0777ac3ae238
Thomas Howarth <T.Howarth2@newcastle.ac.uk>
2023-04-17 15:51:24 +0100

    semicolon

--- Src/XY_ignTab.cpp
+++ Src/XY_ignTab.cpp
@@ -140,7 +140,7 @@ main (int   argc,
     //pp.get("Tb",Tb);
     Real H2Ob, H2u;
     pp.get("H2Ob",H2Ob);
-    pp.get("H2u",H2u)
+    pp.get("H2u",H2u);
     Vector<std::unique_ptr<MultiFab>> outdata(Nlev);
     Vector<Geometry> geoms(Nlev);
     const int nGrow = 0;
